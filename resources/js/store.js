1e4e48150f486fefb3e6c63965891abfe6c8ac4e
Matheus Eufrasio <you@example.com>
2024-09-15 17:33:03 -0300

    refactor: move echo private

--- resources/js/store.js
+++ resources/js/store.js
@@ -8,33 +8,14 @@ export default createStore({
     },
     mutations: {
         serUserState(state, value) {
-            console.log(value)
             state.user = value;
         }
     },
     actions: {
         async userStateAction({ commit }) {
-            await axios.get('api/user/me').then((response) => {
-                const userResponse = response.data.user;
-                commit('serUserState', userResponse);
-
-                // O ponto antes de SendMessage indica que eu não preciso usar todo o namespace dele
-                Echo.private(`user.${userResponse.id}`).listen('.SendMessage', (e) => {
-
-                    console.log('New message event received:', e); // Debug
-
-                    if (this.userActive && this.userActive.id === e.message.from) {
-                        this.messages.push(e.message);
-                        this.scrollToBottom();
-                    } else {
-                        const user = this.users.find((u) => u.id === e.message.from);
-
-                        if (user) {
-                            user.notification = true;
-                        }
-                    }
-                });
-            });
+            const response = await axios.get('api/user/me');
+            const userResponse = response.data.user;
+            commit('serUserState', userResponse);
         }
     },
     plugin: [ createPersistedState() ]
